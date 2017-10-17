def8f2ba9e5e85ea9532200a6a7d317f39e816f9
LmLy <1269458422@qq.com>
2017-10-17 11:17:53 +0800

    Initial Commit

---
+++ VPN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VPN
+//
+//  Created by ly on 2017/10/17.
+//  Copyright © 2017年 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
